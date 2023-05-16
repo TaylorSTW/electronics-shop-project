b74e5a524e53f737dbc60cbf3a877c74cb1002af
TaylorSTW <dmitfrost@ya.ru>
2023-05-16 19:53:11 +0300

    completed 2hw

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,9 +16,39 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
+        Item.all.append(self)
+
+    @property
+    def name(self) -> str:
+        """
+        Getter for attribute "name"
+        """
+        return self.__name
+
+    @name.setter
+    def name(self, new_name: str) -> None:
+        """Set name property of class instance"""
+        try:
+            if len(new_name) <= 10:
+                self.__name = new_name
+            else:
+                raise ValueError
+        except ValueError:
+            print("Name must contain no more than 10 characters")
+
+    @classmethod
+    def instantiate_from_csv(cls) -> None:
+        with open('../src/items.csv', 'r', encoding='windows-1251') as file:
+            cls.all = [] #очистка списков
+            item = csv.DictReader(file)
+            for i in item:
+                name = i["name"]
+                price = cls.string_to_number(i["price"])
+                quantity = cls.string_to_number(i["quantity"])
+                cls(name, price, quantity)
 
     def calculate_total_price(self) -> float:
         """
@@ -30,3 +63,10 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * self.pay_rate
+
+    @staticmethod
+    def string_to_number(any_string: str) -> int:
+        try:
+            return int(any_string)
+        except ValueError:
+            return int(any_string[0: any_string.find('.')])
--- tests/test_item.py
+++ tests/test_item.py
@@ -3,18 +3,69 @@ from src.item import Item
 
 
 @pytest.fixture()
-def tv_obj():
-    return Item("name", 20000, 10)
+def item_book():
+    return Item("Book", 500, 10)
 
 
-def test_function(tv_obj):
-    assert tv_obj.name == "name"
+@pytest.fixture
+def item_bag():
+    return Item("Bag", 1000, 5)
 
 
-def test_calculate_total_price(tv_obj):
-    assert tv_obj.calculate_total_price() == 200000
+def test_show_all(item_book, item_bag):
+    """
+    Checks total number of class instances
+    """
+    assert len(Item.all) == 2
 
 
-def test_apply_discount(tv_obj):
-    tv_obj.apply_discount()
-    assert tv_obj.price == 20000
+def test_item_init(item_book):
+    """
+    Checks correct Item class instance initialization
+    """
+    assert item_book.name == "Book"
+    assert item_book.price == 500
+    assert item_book.quantity == 10
+
+
+def test_calculate_total_price(item_book):
+    """
+    Checks correct calculation of total price of item
+    """
+    assert item_book.calculate_total_price() == 5000
+
+
+def test_apply_discount(item_book):
+    """
+    Checks that correct discount is applied
+    """
+    item_book.pay_rate = 0.65
+    item_book.apply_discount()
+    assert item_book.price == 325
+
+
+def test_string_to_number(item_bag):
+    """
+        Checks conversion of string to integer
+        """
+    assert item_bag.string_to_number('7') == 7
+    assert item_bag.string_to_number('7.0') == 7
+    assert item_bag.string_to_number('7.5') == 7
+
+
+def test_name_getter(item_bag):
+    """
+        Checks name getter
+        """
+    assert item_bag.name == 'Bag'
+
+
+def test_name_setter(item_bag):
+    """
+        Checks name setter
+        """
+    item_bag.name = 'Box'
+    assert item_bag.name == 'Box'
+    # Check exception for >10 chars
+    item_bag.name = 'SuperPuperBag'
+    assert item_bag.name == 'Box'

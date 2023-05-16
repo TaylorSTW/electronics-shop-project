0cc9ae563f64c3c467b32d17a0e1eaeb7ded27e4
TaylorSTW <dmitfrost@ya.ru>
2023-05-16 14:14:24 +0300

    tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,20 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture()
+def tv_obj():
+    return Item("name", 20000, 10)
+
+
+def test_function(tv_obj):
+    assert tv_obj.name == "name"
+
+
+def test_calculate_total_price(tv_obj):
+    assert tv_obj.calculate_total_price() == 200000
+
+
+def test_apply_discount(tv_obj):
+    tv_obj.apply_discount()
+    assert tv_obj.price == 20000
